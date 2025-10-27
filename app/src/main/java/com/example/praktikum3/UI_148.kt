82e78458ee966973fb575cc29893c18534eca41c
Arsyan <syandi.2901@gmail.com>
2025-10-27 04:33:01 -0700

    menambah letak row di file UI

---
+++ app/src/main/java/com/example/praktikum3/UI_148.kt
@@ -0,0 +1,55 @@
+package com.example.praktikum3
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(
+        modifier = Modifier
+            .padding(top=100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text(
+            stringResource(R.string.prodi),
+            fontSize =  35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            stringResource(R.string.univ),
+            fontSize = 22.sp
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+            @@ -46,7 +47,10 @@
+            contentColor = Color.DarkGray
+        )
+        ) {
+        Row() {  }
+        Row() {
+            val gambar = painterResource(R.drawable.umy)
+
+        }
+    }
+    }
